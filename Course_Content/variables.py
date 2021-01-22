900a4d851ca211651e4c8929823c4d1874bc1c13
Obala <obala.reddy017@gmail.com>
2021-01-22 22:49:04 +0530

    SDT: Week 01 , Day 02

---
+++ Course_Content/variables.py
@@ -0,0 +1,20 @@
+my_name = "Aryan"
+print(my_name)
+close_price = 30510
+print(close_price)
+adjusted_close = 30590.53
+print(adjusted_close)
+#Make CAGR calculation, not absolute return over a period of time
+wonder = 1000
+print(wonder/1000)
+a = b = c = 100
+d, e, f = 200, 201, 202
+g, h, i = 300, 400, 500
+print(a, b, c, d, e, f, g, h, i)
+
+#a = b == 30
+#print(a, b)
+
+nocg = 45
+Nocg = 45
+print(nocg, Nocg)
