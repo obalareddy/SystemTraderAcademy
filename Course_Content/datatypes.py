0a6b8126e52ceb53b89a0e3a2966e33ab1bb3311
Obala <obala.reddy017@gmail.com>
2021-01-22 23:59:42 +0530

    SDT: Week 01, Day 03- Data types

---
+++ Course_Content/datatypes.py
@@ -0,0 +1,10 @@
+ticker = "NIFTY_F1"
+today_date = "22/01/2021"
+today_time = "09:15:00"
+open_price = 13510.14
+high_price = 13670.34
+low_price = 13255.00
+close_price = 13317.00
+volume = 250000
+
+print(type(open_price))
