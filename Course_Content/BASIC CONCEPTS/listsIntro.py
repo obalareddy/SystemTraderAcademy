656f096009b8e9126f031ee724f8745dd20abce3
Obala <obala.reddy017@gmail.com>
2021-01-27 23:52:13 +0530

    STA: Week 02, Day 8 - Lists

---
+++ Course_Content/BASIC CONCEPTS/listsIntro.py
@@ -0,0 +1,23 @@
+#Tuple
+names = "Virat", "Rohith", "Pant", "Gill"
+print(names)
+#lists
+names_list = ["Virat", "Rohith", "Pant", "Gill"]
+print(names_list)
+
+# Tuple is called as immutable or not changable 
+# List is mutable
+
+#allowed
+names_list[0] = "Virat Kohli"
+print(names_list[0])
+
+#We cannot add any elements to tuple
+
+#List Functions
+'''
+1. append
+2. copy
+3. clear
+4. count(1)
+'''
